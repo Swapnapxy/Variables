5d60f102a07b30a6d73c243b1010aa49acefcf48
Swapnapxy <146064964+Swapnapxy@users.noreply.github.com>
2023-09-26 12:35:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var score=0;
+function score2(){
+    score=score-10;
+    document.getElementById("sc").textContent=score;
+    
+}
+function score1(){
+    score=score+10;
+    document.getElementById("sc").textContent=score;
+}
